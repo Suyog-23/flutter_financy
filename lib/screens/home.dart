5e8063b9906141441b6e4f62b63a4af0fd74918f
Suyog-23 <suyog231002@gmail.com>
2021-08-19 12:59:30 +0530

    section and home setup

---
+++ lib/screens/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  HomeState createState()=> HomeState();
+}
+
+class HomeState extends State<Home>{
+  @override 
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
